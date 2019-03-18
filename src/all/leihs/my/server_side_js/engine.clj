ed8b930e257fa6a88716f93e3deaf2d2da4ac2c6
Thomas Schank <DrTom@schank.ch>
2019-03-18 14:44:50 +0100

    WIP prepare for int testing (squashed!)

--- src/all/leihs/my/server_side_js/engine.clj
+++ src/all/leihs/my/server_side_js/engine.clj
@@ -2,17 +2,29 @@
   (:require
     [me.raynes.conch :refer [programs]]
     [clojure.tools.logging :as log]
+    [clojure.java.io :as io]
     [leihs.core.json :refer [to-json]]))
 
 (programs node)
 
 (def render-wait-time-max-ms 5000)
 
+(def tmp-file "/tmp/leihs-ui-server-side.js")
+
+(defn server-side-js-input-stream []
+  (-> "public/my/leihs-ui-server-side.js"
+      io/resource io/input-stream))
+
+(with-open [in (server-side-js-input-stream)]
+  (io/copy in (io/file tmp-file)))
+
+;(slurp (server-side-js-input-stream))
+
 (defn js-code
   [name props]
   (str
     "window = global = this
-   l = require('./leihs-ui/dist/leihs-ui-server-side')
+   l = require('" tmp-file "')
    l.renderComponentToString('"
     name
     "', "

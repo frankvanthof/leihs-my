0c3ced5d43bc5133173e37788ad2defb3659c6e0
Max Albrecht <max.albrecht@zhdk.ch>
2019-03-19 18:44:29 +0100

    wip

--- src/all/leihs/my/server_side_js/engine.clj
+++ src/all/leihs/my/server_side_js/engine.clj
@@ -9,23 +9,32 @@
 
 (def render-wait-time-max-ms 5000)
 
+(defn ssr-cli []
+  (-> "public/my/leihs-ssr.js" io/resource))
+
 (defn ssr-cli-input-stream []
-  (-> "public/my/leihs-ssr.js"
-  io/resource io/input-stream))
+  (-> "public/my/leihs-ssr.js" io/resource io/input-stream))
 
 ; TMP FOR POC. But we might want to do it thusly in dev mode, so it can be watched?
-(programs cat)
-(def tmp-file "/tmp/leihs-ssr.js")
-(with-open [in (ssr-cli-input-stream)]
-  (io/copy in (io/file tmp-file)))
+; (programs cat)
+; (def tmp-file "/tmp/leihs-ssr.js")
+; (with-open [in (ssr-cli-input-stream)]
+;   (io/copy in (io/file tmp-file)))
 ; this should just be the input stream, but just piping it did not work.
-(defn ssr-cli [] (cat tmp-file))
+; (defn ssr-cli [] (cat tmp-file))
 
 (defn render-react
   [name props]
   (try
-    (node
-      "-" "render" name (to-json props)
-      {:in (ssr-cli) :timeout render-wait-time-max-ms})
+    ; (node
+    ;   "-" "render" name (to-json props)
+    ;   {:in (ssr-cli-input-stream) :timeout render-wait-time-max-ms})
+
+    ; DEBUG: see if source of script is printed
+    (programs cat)
+    (cat
+      "-"
+      {:in (ssr-cli-input-stream) :timeout render-wait-time-max-ms})
+
     (catch Exception e
       (throw (ex-info "Render Error!" {:status 500, :causes {:err e}})))))
